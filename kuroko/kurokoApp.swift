60daaef66852c9b54e0be5845eea9adfa8142433
Eisuke Hayashi <eiuske_space0527@icloud.com>
2025-12-08 19:25:27 +0900

    Initial Commit

---
+++ kuroko/kurokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kurokoApp.swift
+//  kuroko
+//
+//  Created by 林栄介 on 2025/12/08.
+//
+
+import SwiftUI
+
+@main
+struct kurokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kurokoTests/kurokoTests.swift
@@ -0,0 +1,16 @@
+//
+//  kurokoTests.swift
+//  kurokoTests
+//
+//  Created by 林栄介 on 2025/12/08.
+//
+
+import Testing
+
+struct kurokoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
